e88b6a465f9ec3584ee6bf96cf7bed581f5edf2c
Felix Wöstmann <git@lostincoding.de>
2021-02-27 14:32:46 +0100

    Restructured mail_service

--- mail_service.py
+++ mail_service.py
@@ -1,6 +1,8 @@
 import sendgrid
 import os
 from sendgrid.helpers.mail import *
+from stocklevels import *
+from geo_service import *
 
 
 def send_mail(to_mail, from_mail, subject, message):
@@ -12,4 +14,35 @@ def send_mail(to_mail, from_mail, subject, message):
     response = sg.client.mail.send.post(request_body=mail.get())
 
 
+def prepare_update_message(address, stores):
+    stores_with_gold = [store for store in stores if store.get_stocklevel(KODAK_GOLD).stocklevel > 0]
+    stores_with_colorplus = [store for store in stores if store.get_stocklevel(KODAK_COLORPLUS).stocklevel > 0]
+    stores_with_utramax = [store for store in stores if store.get_stocklevel(KODAK_ULTRAMAX).stocklevel > 0]
+    string_list = ["In folgenden Stores sind in deiner Nähe wieder Kodak Filme verfügbar!"]
+    if stores_with_gold:
+        string_list.append("")
+        string_list.append("Kodak Gold 3er")
+        for store in stores_with_gold:
+            string_list.append(
+                f"Entfernung: {calculate_distance_address_store(address, store) :.1f} km "
+                f"Stadt: {store.address['city']} Bestand: {store.get_stocklevel(KODAK_GOLD).stocklevel} "
+                f"Packungen {name_for_product_number.get(KODAK_GOLD, 'Not Found')}")
 
+    if stores_with_colorplus:
+        string_list.append("")
+        string_list.append("Kodak Colorplus")
+        for store in stores_with_colorplus:
+            string_list.append(
+                f"Entfernung: {calculate_distance_address_store(address, store) :.1f} km "
+                f"Stadt: {store.address['city']} Bestand: {store.get_stocklevel(KODAK_COLORPLUS).stocklevel} "
+                f"Packungen {name_for_product_number.get(KODAK_COLORPLUS, 'Not Found')}")
+
+    if stores_with_utramax:
+        string_list.append("")
+        string_list.append("Kodak Ultramax")
+        for store in stores_with_utramax:
+            string_list.append(
+                f"Entfernung: {calculate_distance_address_store(address, store) :.1f} km "
+                f"Stadt: {store.address['city']} Bestand: {store.get_stocklevel(KODAK_ULTRAMAX).stocklevel} "
+                f"Packungen {name_for_product_number.get(KODAK_ULTRAMAX, 'Not Found')}")
+    return "\n".join(string_list)
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-address = input("Bitte Stadt eingeben:  ")
 from stores import *
 from stocklevels import *
 from geo_service import *
+import argparse
 
 
 def print_distance_stores_with_product(address, stores, product_number):
@@ -19,38 +19,7 @@ def print_distance_stores_with_product(address, stores, product_number):
             f"Entfernung: {tuple[1]:.1f} km Stadt: {tuple[0].address['city']} Bestand: {tuple[0].get_stocklevel(product_number).stocklevel} Packungen {name_for_product_number.get(product_number, 'Not Found')}")
 
 
-def prepare_update_message(address, stores):
-    stores_with_gold = [store for store in stores if store.get_stocklevel(KODAK_GOLD).stocklevel > 0]
-    stores_with_colorplus = [store for store in stores if store.get_stocklevel(KODAK_COLORPLUS).stocklevel > 0]
-    stores_with_utramax = [store for store in stores if store.get_stocklevel(KODAK_ULTRAMAX).stocklevel > 0]
-    string_list = ["In folgenden Stores sind in deiner Nähe wieder Kodak Filme verfügbar!"]
-    if stores_with_gold:
-        string_list.append("")
-        string_list.append("Kodak Gold 3er")
-        for store in stores_with_gold:
-            string_list.append(
-                f"Entfernung: {calculate_distance_address_store(address, store) :.1f} km "
-                f"Stadt: {store.address['city']} Bestand: {store.get_stocklevel(KODAK_GOLD).stocklevel} "
-                f"Packungen {name_for_product_number.get(KODAK_GOLD, 'Not Found')}")
 
-    if stores_with_colorplus:
-        string_list.append("")
-        string_list.append("Kodak Colorplus")
-        for store in stores_with_colorplus:
-            string_list.append(
-                f"Entfernung: {calculate_distance_address_store(address, store) :.1f} km "
-                f"Stadt: {store.address['city']} Bestand: {store.get_stocklevel(KODAK_COLORPLUS).stocklevel} "
-                f"Packungen {name_for_product_number.get(KODAK_COLORPLUS, 'Not Found')}")
-
-    if stores_with_utramax:
-        string_list.append("")
-        string_list.append("Kodak Ultramax")
-        for store in stores_with_utramax:
-            string_list.append(
-                f"Entfernung: {calculate_distance_address_store(address, store) :.1f} km "
-                f"Stadt: {store.address['city']} Bestand: {store.get_stocklevel(KODAK_ULTRAMAX).stocklevel} "
-                f"Packungen {name_for_product_number.get(KODAK_ULTRAMAX, 'Not Found')}")
-    return "\n".join(string_list)
 
 
 def populate_stores_with_all_stocks(stores):
@@ -70,10 +39,14 @@ def filter_stores_for_distance(address, stores, radius):
     return [tup[0] for tup in sorted_by_distance if tup[1] <= radius]
 
 
+
+
 if __name__ == '__main__':
-    stores = load_stores_from_file("german_stores.json")
-    print_distance_stores_with_product(address, stores, KODAK_GOLD)
-    #print_distance_stores_with_product(address, stores, KODAK_COLORPLUS)
-    #print_distance_stores_with_product(address, stores, KODAK_ULTRAMAX)
+    address, radius, filmtypes = cli()
+
+    # stores = load_stores_from_file("german_stores.json")
+    # print_distance_stores_with_product(args.Address, stores, KODAK_GOLD)
+    # print_distance_stores_with_product(address, stores, KODAK_COLORPLUS)
+    # print_distance_stores_with_product(address, stores, KODAK_ULTRAMAX)
     # stores_in30km_with_stocks = filter_stores_for_distance(populate_stores_with_all_stocks(stores), 40)
     # print(prepare_update_message(stores_in30km_with_stocks))
